d8d5d6483fd4fdfefd6150fb2fcc1adea22cef41
arpitkatiyar108 <arpitkatiyar108@gmail.com>
2025-05-11 17:44:55 +0530

    Update String_Q1.py

--- String_Q1.py
+++ String_Q1.py
@@ -1,3 +1,4 @@
+# Write a Python program to remove a newline in Python String = "\nBest \nDeeptech \nPython \nTraining\n"
 text = "\nBest \nDeeptech \nPython \nTraining\n"
 cleaned_text = text.replace("\n", "")
 print(cleaned_text)
